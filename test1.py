ca0bbe29c73da86dcadfb777faf9b0830e9fc3fa
DanielRuch <44618830+DanielRuch@users.noreply.github.com>
2019-10-26 13:27:02 -0500

    Changed test1.py with notes, added directory for practice

---
+++ test1.py
@@ -0,0 +1,9 @@
+#This is a test file for pushing changes to github.
+# Remember: git add .
+#       - adds tracking to ALL files in folder. Can specify file if only want to track one/add one to tracking
+#git status
+#       -Gives the status of the folder, as well as listing changes yet to be committed
+#git commit -m "Your note"
+#       -Commits changes, -m specifies to add a reason and the stuff in quotation is the actual note message
+#git push origin master
+#       -Actually pushes to the target branch, in this case master.
